d6a66385af639fe6113b6d4904ac8e1b319b3914
Everton-Buenno <evertonbueno2014@gmail.com>
2022-05-30 02:58:58 -0300

    login bd

--- App/BD.php
+++ App/BD.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "sistema_de_registro";
+$server = "us-cdbr-east-05.cleardb.net";
+$user = "b49fba34879a93";
+$pass = "cfea3a75";
+$db_name = "heroku_4c66442df93ff8c";
 
 
 try {
